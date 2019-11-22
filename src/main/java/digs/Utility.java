65908ab46f70f47f75fb1a7fbf807970e9e1d0bc
Hassan Nazeer Chaudhry <HASSANNAZEER.CHAUDHRY@POLIMI.IT>
2019-11-22 18:55:13 +0100

    Initial Boiler plate code

---
+++ src/main/java/digs/Utility.java
@@ -0,0 +1,27 @@
+package digs;
+
+public class Utility {
+
+  public boolean checkGlobalOptimal() {
+      return false;
+  }
+
+  public boolean checkLocalOptimal() {
+        return false;
+    }
+
+
+    public boolean isBlockedLocal() {
+        return false;
+    }
+
+
+    public boolean isBlockedGlobal() {
+        return false;
+    }
+
+    public void calGlobalOptimal(){
+
+    }
+
+}
