7068974e344d14b2b55d5ee5de300572d0093ded
Hassan Nazeer Chaudhry <HASSANNAZEER.CHAUDHRY@POLIMI.IT>
2019-11-22 19:28:50 +0100

    Added Sorting and Cost Matrix Generation

--- src/main/java/DIGSTest.java
+++ src/main/java/DIGSTest.java
@@ -1,2 +1,34 @@
+import digs.Sort2DMatrix;
+
+import java.util.Random;
+
 public class DIGSTest {
+    public static void main(String[] args) {
+
+        int paramSize=20; //Number of total contraints
+        int constSize=5; // number of constraints
+        int [][] costMat = new int [5][20];
+
+
+
+        System.out.print("Cost Matrix\n ");
+        //Generating the cost matrix
+        for(int i = 0; i < constSize; i++){ // 1 2 3 4 5
+            for(int j = 0; j <paramSize; j++) { // 1 2 3 4 5
+                Random r = new Random();
+                costMat[i][j] =  r.nextInt(100) ;
+                if (costMat[i][j]<10) {
+                    System.out.print(" " + costMat[i][j] + " ");
+                } else {
+                    System.out.print(costMat[i][j] + " ");
+                }
+            }//end of for J
+            System.out.println();
+        }//end of for i
+
+        
+        System.out.print("Sorted Array\n ");
+        Sort2DMatrix.sortRowWise(costMat);
+
+    }
 }
